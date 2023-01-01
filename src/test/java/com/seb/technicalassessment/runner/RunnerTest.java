c49f1b06885c1a8823cc9ac95a21dde6927117d1
alyaa112 <95202193+alyaa112@users.noreply.github.com>
2023-01-02 06:06:18 +0800

    automation002 - test

--- src/test/java/com/seb/technicalassessment/runner/RunnerTest.java
+++ src/test/java/com/seb/technicalassessment/runner/RunnerTest.java
@@ -1,13 +1,9 @@
 package com.seb.technicalassessment.runner;
 
-import io.cucumber.testng.CucumberOptions;
-import io.cucumber.testng.FeatureWrapper;
-import io.cucumber.testng.PickleWrapper;
+import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.TestNGCucumberRunner;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
 
 @CucumberOptions(
         plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json", "html:target/cucumber-reports/Cucumber.html",

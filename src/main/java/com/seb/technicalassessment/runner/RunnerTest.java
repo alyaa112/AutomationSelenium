fa98c1eb591d52b0787aa0f5e45721f656c04c22
alyaa112 <95202193+alyaa112@users.noreply.github.com>
2023-01-02 03:15:48 +0800

    automation002 - fix step

--- src/main/java/com/seb/technicalassessment/runner/RunnerTest.java
+++ src/main/java/com/seb/technicalassessment/runner/RunnerTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
     @CucumberOptions(
         plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json", "html:target/cucumber-reports", "junit:target/cucumber-reports/Cucumber.xml"},
         features = {"src/main/feature/"},
-        glue = {"com.dahmakan.webapp.stepdefinition"},
+        glue = {"com.seb.technicalassessment.stepdefinition"},
         tags = "@SEBTest",
         monochrome = true
     )

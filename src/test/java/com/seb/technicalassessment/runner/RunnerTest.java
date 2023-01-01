a831875d99911f8f70ced6a6ef36086dca1c6b7f
alyaa112 <95202193+alyaa112@users.noreply.github.com>
2023-01-02 06:19:01 +0800

    automation002 - test junit runwith tag

--- src/test/java/com/seb/technicalassessment/runner/RunnerTest.java
+++ src/test/java/com/seb/technicalassessment/runner/RunnerTest.java
@@ -1,7 +1,10 @@
 package com.seb.technicalassessment.runner;
 
+import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
+@RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json", "html:target/cucumber-reports/Cucumber.html",
                 "junit:target/cucumber-reports/Cucumber.xml"},

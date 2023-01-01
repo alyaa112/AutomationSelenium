333558fb4738b6f0ff8da50f7d26d48d253fc5e5
alyaa112 <95202193+alyaa112@users.noreply.github.com>
2023-01-02 03:20:47 +0800

    automation002 - add tags

--- src/main/java/com/seb/technicalassessment/runner/RunnerTest.java
+++ src/main/java/com/seb/technicalassessment/runner/RunnerTest.java
@@ -1,8 +1,14 @@
 package com.seb.technicalassessment.runner;
 
 import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.FeatureWrapper;
+import io.cucumber.testng.PickleWrapper;
+import io.cucumber.testng.TestNGCucumberRunner;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-    @CucumberOptions(
+@CucumberOptions(
         plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json", "html:target/cucumber-reports", "junit:target/cucumber-reports/Cucumber.xml"},
         features = {"src/main/feature/"},
         glue = {"com.seb.technicalassessment.stepdefinition"},
@@ -11,4 +17,21 @@ import io.cucumber.junit.CucumberOptions;
     )
 
 public class RunnerTest {
+    private TestNGCucumberRunner testNGCucumberRunner;
+
+    @BeforeClass(alwaysRun = true)
+    public void setup() {
+        testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+    }
+
+    @Test(dataProvider = "feature")
+    public void features(PickleWrapper eventWrapper, FeatureWrapper cucumberFeature) throws Throwable {
+        testNGCucumberRunner.runScenario(eventWrapper.getPickle());
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDownClass() {
+        testNGCucumberRunner.finish();
+    }
+
 }

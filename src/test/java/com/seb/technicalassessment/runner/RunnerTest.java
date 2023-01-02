80484a2d6d4e04dcc4da6df8a20c2e8efd0b17fd
alyaa112 <95202193+alyaa112@users.noreply.github.com>
2023-01-02 17:22:26 +0800

    automation002 - remove testng dep

--- src/test/java/com/seb/technicalassessment/runner/RunnerTest.java
+++ src/test/java/com/seb/technicalassessment/runner/RunnerTest.java
@@ -1,20 +1,18 @@
 package com.seb.technicalassessment.runner;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty", "json:target/cucumber-reports/Cucumber.json", "html:target/cucumber-reports/Cucumber.html",
                 "junit:target/cucumber-reports/Cucumber.xml"},
         features = {"src/test/feature/"},
-        glue = {"com.seb.technicalassessment.stepdefinition"},
+        glue = {"src/test/java/com/seb/technicalassessment/stepdefinition"},
         tags = "@SEBTest",
         monochrome = true
     )
 
 public class RunnerTest {
-
-
 }
